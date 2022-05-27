a3ca72d85e9685612e0dab0ac0d6c690341324f8
Nate Brandeburg <brandeburgnate@gmail.com>
2022-05-27 14:57:29 -0500

    Show health as int
    I'm bad at animation and don't know how to make the health bar work, yet

--- Scenes/HUD/HUD.gd
+++ Scenes/HUD/HUD.gd
@@ -2,14 +2,15 @@ extends CanvasLayer
 
 export (Resource) var Health
 
-onready var healthbar = $HealthBar
+onready var health_label = $Health
 
 func _ready():
 	if Health:
-		Health.CONNECT_DEFERRED("health_changed", self, "_on_player_health_changed")
+		Health.connect("health_changed", self, "_on_player_health_changed")
+		health_label.text = str(Health.max_value)
 		
 func _on_player_health_changed(value):
-	healthbar.value = float(value) / Health.max_value * 100
+	health_label.text = str(float(value) / Health.max_value * 100)
 
 func set_text(text):
 	$Label.text	= str(text)
