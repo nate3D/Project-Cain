e81b59f24d8550f78559b564d36f1a073feec46c
Nate Brandeburg <brandeburgnate@gmail.com>
2022-05-28 11:10:40 -0500

    Addition of remaining dark assets

---
+++ Assets/Dark/Tilesets/Ancient Caves/Aseprite and Tiled Files/Tileset Add on.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.2" name="Tileset Add on" tilewidth="16" tileheight="16" tilecount="576" columns="24">
+ <image source="Tileset Add on.png" width="384" height="384"/>
+</tileset>
---
+++ Assets/Dark/Tilesets/Dark City/Tiled Files/Dark City Test.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.2" name="Dark City Test" tilewidth="16" tileheight="16" tilecount="400" columns="20">
+ <image source="City Tileset.png" width="320" height="320"/>
+</tileset>
